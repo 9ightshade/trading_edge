8823b48a6dfc3be27d5cd7a0829b7c9f64086742
9ightshade <aniekemeusanga@gmail.com>
2024-12-25 06:51:30 +0100

    sidebar complete

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
-
+import logo from "../assets/logo.svg"
+import logout from "../assets/logout.png"
 export const Sidebar = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='bg-[#0D0D0D] py-4 px-4 h-[100vh] relative w-2/12' >
+            <div className='logo flex items-center gap-2 '  >
+                <img src={logo} alt="logo" />
+                <span className='text-white font-bold' >
+                    Trading Edge
+                </span>
+            </div>
+
+            <div className='main flex flex-col gap-2 text-white mt-7' >
+                <p>
+                    <button className='hover:text-green-400 bg-gray-900 py-2 px-5 rounded-full w-full' >
+                        Dashboard
+                    </button>
+                </p>
+                <p>
+                    <button className='hover:text-green-400 bg-gray-900 py-2 px-5 rounded-full w-full'>
+                        WatchList
+                    </button>
+                </p>
+                <p>
+                    <button className='hover:text-green-400 bg-gray-900 py-2 px-5 rounded-full w-full'>
+                        Alerts
+                    </button>
+                </p>
+            </div>
+
+
+
+            <div className='footer text-white absolute bottom-0 left-0 p-4' >
+                <button className='flex gap-2 items-center hover:text-green-400 ' >
+                    <img src={logout} alt="logout" />  <span>
+                        Log out
+                    </span>
+                </button>
+            </div>
+        </div>
+    )
 }
