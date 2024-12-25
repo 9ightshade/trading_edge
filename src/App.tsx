05a16bb0cb62df83b182b5733c23949698a662fa
9ightshade <aniekemeusanga@gmail.com>
2024-12-25 06:14:02 +0100

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import Home from "./pages/home"
 
 function App() {
 
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">
-        Hello world!
-      </h1>
+      <Home />
     </>
   )
 }
